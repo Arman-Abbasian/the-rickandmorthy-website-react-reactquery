f5459298b92be82b3a5e5c809419375ae0355d47
Arman Abbasian <abasian.arman@gmail.com>
2023-11-12 17:29:33 +0330

    develope the Character component

--- src/components/Characters.tsx
+++ src/components/Characters.tsx
@@ -1,8 +1,42 @@
 
 function Characters() {
   return (
-    <div>Characters</div>
+    <div className="flex flex-wrap gap-4">
+      <Character />
+      <Character />
+      <Character />
+      <Character />
+    </div>
   )
 }
 
 export default Characters
+
+import { FcBusinesswoman, FcBusinessman } from "react-icons/fc";
+import { AiOutlineEye } from "react-icons/ai";
+
+function Character() {
+  return (
+    <div className='flex h-20 rounded-md overflow-hidden text-xs bg-blue-200'>
+      {/*! image section */}
+      <div>
+        <img className='w-full h-full object-cover' src='https://rickandmortyapi.com/api/character/avatar/2.jpeg' />
+      </div>
+      {/*! info section */}
+      <div className='flex-1 flex justify-between items-center gap-16  px-3'>
+        <div className='flex flex-col justify-between gap-5'>
+        <div className='flex gap-1 items-center'>
+        <FcBusinessman className="mobile-icon hover:cursor-pointer"/>
+          <p> ali meira</p>
+        </div>
+        <div className='flex gap-1 items-center'>
+          <span className="w-2 h-2 rounded-full bg-green-500"></span>
+          <p>Alive-</p>
+          <p>human</p>
+        </div>
+        </div>
+        <AiOutlineEye className="mobile-icon hover:cursor-pointer"/>
+      </div>
+    </div>
+  )
+}
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,7 +3,7 @@ function Layout() {
   return (
     <div className="flex gap-8 p-4">
         <Navbar />
-        <div className='flex-1 ml-[70px] bg-red-100 px-10'>
+        <div className='flex-1 ml-[66px]  px-10'>
         <Outlet />
         </div>
     </div>
@@ -14,19 +14,33 @@ export default Layout
 
 import { HiOutlineFilm } from "react-icons/hi";
 import { BsPerson } from "react-icons/bs";
+import React, { useState } from 'react';
 function Navbar() {
   return (
         <aside className="w-16 flex justify-center fixed left-4 top-4 h-screen  bg-slate-700 overflow-y-auto rounded-tl-md rounded-bl-md" >
             <ul className="flex flex-col gap-6 justify w-full ">
-                <li className="flex justify-center">
-                    <NavLink className='hover:bg-cyan-300 cursor-pointer w-full flex justify-center p-4' to={''}><HiOutlineFilm  /></NavLink>
-                </li>
-                <li className="flex justify-center">
-                <NavLink className='hover:bg-cyan-300 cursor-pointer w-full flex justify-center p-4' to={'characters'}><BsPerson /></NavLink>
-                </li>
-                <li className="hover:bg-cyan-300 cursor-pointer flex justify-center p-4">3</li>
+                <SidebarItem text='episodes' to=''><HiOutlineFilm   /></SidebarItem>
+                <SidebarItem text='characters' to='characters'><BsPerson /></SidebarItem>
             </ul>
         </aside>
+  )
+}
+
+
+
+interface IProps{
+  children:React.ReactNode;
+  to:string;
+  text:string
+
+}
+
+function SidebarItem({children,to,text}:IProps) {
+  const [showText,setShowText]=useState(false)
+  return (
+    <li className="flex justify-center" onMouseOver={()=>setShowText(true)} onMouseOut={()=>setShowText(false)}>
+                    <NavLink className='hover:bg-cyan-300 cursor-pointer w-full flex justify-center p-4' to={to}>{children} <p className={`${showText?'block':'hidden'} text-xs`}>{text}</p></NavLink>
+                </li>
   )
 }
 
