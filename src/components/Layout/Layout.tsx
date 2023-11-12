d18eff5d71e219d623c4e405f17af1cc3d352526
Arman Abbasian <abasian.arman@gmail.com>
2023-11-12 13:46:36 +0330

    code the sidebar section

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -12,18 +12,19 @@ function Layout() {
 
 export default Layout
 
-
+import { HiOutlineFilm } from "react-icons/hi";
+import { BsPerson } from "react-icons/bs";
 function Navbar() {
   return (
-        <aside className="w-15 fixed left-4 top-4 h-screen p-2 bg-slate-700 overflow-y-auto rounded-tl-md rounded-bl-md" >
-            <ul className="">
-                <li className=''>
-                    <NavLink to={''}>episode</NavLink>
+        <aside className="w-16 flex justify-center fixed left-4 top-4 h-screen  bg-slate-700 overflow-y-auto rounded-tl-md rounded-bl-md" >
+            <ul className="flex flex-col gap-6 justify w-full ">
+                <li className="flex justify-center">
+                    <NavLink className='hover:bg-cyan-300 cursor-pointer w-full flex justify-center p-4' to={''}><HiOutlineFilm  /></NavLink>
                 </li>
-                <li>
-                <NavLink to={'characters'}>characters</NavLink>
+                <li className="flex justify-center">
+                <NavLink className='hover:bg-cyan-300 cursor-pointer w-full flex justify-center p-4' to={'characters'}><BsPerson /></NavLink>
                 </li>
-                <li>3</li>
+                <li className="hover:bg-cyan-300 cursor-pointer flex justify-center p-4">3</li>
             </ul>
         </aside>
   )
