b8c30faf5c8799da36ef2888bafebb9dd8207a57
Arman Abbasian <abasian.arman@gmail.com>
2023-11-14 10:59:06 +0330

    some refactor on codes

--- src/components/Characters.tsx
+++ src/components/Characters.tsx
@@ -16,15 +16,21 @@ const fetchCharacters=async(queryFilters:string)=>{
 }
 
 function Characters() {
+  //! initial filter query string =>is used when user back to website after a while
   const initialStatus:string= localStorage.getItem("statusFilter") ? localStorage.getItem("statusFilter")! :"";
   const initialGender:string= localStorage.getItem("genderFilter") ? localStorage.getItem("genderFilter")! :"";
 
+  //! 3 state to handle the change of 3 querystring filter 
   const [page,setPage]=useState<number>(1)
   const [statusFilter,setStatusFilter]=useState<string>(initialStatus);
   const [genderFilter,setGenderFilter]=useState<string>(initialGender);
+  //!this useEffect handle the change of all pages when change the status and gender state
+  //!and setPages to 1 for pretend the error
 useEffect(()=>{
   setPage(1)
 },[statusFilter,genderFilter])
+
+//!store the changes of statusFilter & genderFilter states in localStorage
   if(statusFilter){
     localStorage.setItem("statusFilter",statusFilter)
   }else {
@@ -37,13 +43,14 @@ useEffect(()=>{
   if(statusFilter) searchParams.status=statusFilter
   if(genderFilter) searchParams.gender=genderFilter
 
+  //! quryFilters is all the quryString that we want to stick to the url
   const queryFilters:string=queryString.stringify(searchParams);
   
-  const { isLoading, isError, data,error } = useQuery({
+  //! useQuery function
+  const { isLoading, isError, data } = useQuery({
     queryKey: ['characters',page,statusFilter,genderFilter],
     queryFn:()=> fetchCharacters(queryFilters),
   });
-  console.log(error)
 
   const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
     setPage(value)
@@ -55,10 +62,12 @@ useEffect(()=>{
     <div>
       <Navigate to={`/characters/?${queryFilters}`} />
       <FilterCharacter genderFilter={genderFilter} setGenderFilter={setGenderFilter} statusFilter={statusFilter} setStatusFilter={setStatusFilter} />
+      <div>
     <div className="flex flex-wrap justify-center gap-4 ">
       {data.data.results.map((item:ICharacter)=>{
         return <Character character={item} /> 
       })}
+      </div>
       <div className='flex justify-center items-center mt-16'>
     <Pagination color="primary" MuiPaginationItem-textSecondary count={data.data.info.pages} page={page} onChange={handleChange} />
       </div>
@@ -69,6 +78,8 @@ useEffect(()=>{
 
 export default Characters
 
+
+//! one character item
 import { FcBusinesswoman, FcBusinessman } from "react-icons/fc";
 import { AiOutlineEye } from "react-icons/ai";
 import { ICharacter } from "../generalTypes";
@@ -87,14 +98,14 @@ function Character({character}:IProps) {
         <img className='w-full h-full object-cover' src={character.image} alt={character.name} />
       </div>
       {/*! info section */}
-      <div className='flex-1 flex justify-between items-center gap-16  px-3'>
-        <div className='flex flex-col justify-between gap-5'>
+      <div className='flex-1 flex justify-between items-center px-3'>
+        <div className='flex-1 flex flex-col justify-between gap-5'>
         <div className='flex gap-1 items-center'>
           {character.gender==="Male"?<FcBusinessman className="mobile-icon hover:cursor-pointer"/>
           :<FcBusinesswoman className="mobile-icon hover:cursor-pointer"/>
           }
         
-          <p>{character.name}</p>
+          <p className={`text-ellipsis overflow-hidden`}>{character.name.length>20 ? character.name.substring(0,20)+"...":character.name}</p>
         </div>
         <div className='flex gap-1 items-center'>
           <span className="w-2 h-2 rounded-full bg-green-300"></span>
